0ec2db9b0f8bc694037aa3fe265f91d10e398538
Hardik Sarkar <107950304+hardiksarkar@users.noreply.github.com>
2023-08-11 15:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	if(key=="red" || key=="white"){
+		return true;
+	}
+	return false;
 }
 
 // Do not change the code below
